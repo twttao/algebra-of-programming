3e6b95745cf035bbf49c831d6f3677a7593a193a
Tao Tao <ttao@thoughtworks.com>
2018-03-27 00:56:17 +0800

    add factorial & fib

--- NaturalNumbers.hs
+++ NaturalNumbers.hs
@@ -13,3 +13,17 @@ mult m n = foldN 0 (plus n) m
 
 expn:: Int -> Int -> Int
 expn m n = foldN 1 (mult m) n
+
+-- factorial
+fact:: Int -> Int
+fact n = snd (foldN (0, 1) f n) where
+    f:: (Int, Int) -> (Int, Int)
+    f (m, n) = (m + 1, (m + 1) * n)
+-- (0, 1) -> (1, 1 * 1) -> (2, 2 * 1 * 1) -> (3, 3 * 2 * 1 * 1) -> ...
+-- fact 0 ->   fact 1   ->     fact 2     ->        fact 3      -> ...
+
+-- Fibonacci
+fib:: Int -> Int
+fib n = snd (foldN (0, 1) f n) where
+    f:: (Int, Int) -> (Int, Int)
+    f (m, n) = (n, m + n)
