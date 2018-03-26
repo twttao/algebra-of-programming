6e451eb46104bf81fff37609bb5db27e17a50d1c
Tao Tao <ttao@thoughtworks.com>
2018-03-27 00:46:41 +0800

    define functions using foldN

--- NaturalNumbers.hs
+++ NaturalNumbers.hs
@@ -1,5 +1,15 @@
 module NaturalNumbers where
 
+-- foldN = homomorphism over Nat
 foldN:: a -> (a -> a) -> Int -> a
 foldN c h 0 = c     -- c replaces Z
 foldN c h n = h (foldN c h (n - 1))   -- h replaces S
+
+plus:: Int -> Int -> Int
+plus m n = foldN m (+1) n
+
+mult:: Int -> Int -> Int
+mult m n = foldN 0 (plus n) m
+
+expn:: Int -> Int -> Int
+expn m n = foldN 1 (mult m) n
