d8222883d01f70230139a62a0e3614d041e79ad0
Tao Tao <ttao@thoughtworks.com>
2018-03-29 00:25:48 +0800

    add fold and concat

--- Lists.hs
+++ Lists.hs
@@ -28,3 +28,22 @@ convert (Snoc al a) = snocr (convert al) a where
 listR:: (a -> b) -> Listr a -> Listr b
 listR f Nilr = Nilr
 listR f (Cons a al) = Cons (f a) (listR f al)
+
+-- fold / reduce
+foldR:: b -> (a -> b -> b) -> Listr a -> b
+foldR c h Nilr = c      -- c replaces Nilr
+foldR c h (Cons b bl) = h b (foldR c h bl)      -- h replaces Cons
+
+foldL:: b -> (b -> a -> b) -> Listl a -> b
+foldL c h Nill = c      -- c replaces Nill
+foldL c h (Snoc bl b) = h (foldL c h bl) b      -- h replaces Snoc
+
+-- use fold to define concatenation
+catl:: Listl a -> Listl a -> Listl a
+catl la = foldL la Snoc
+
+catR:: Listr a -> Listr a -> Listr a
+catR la = \la2 -> foldR la2 Cons la
+
+concatR:: Listr (Listr a) -> Listr a
+concatR = foldR Nilr catR
