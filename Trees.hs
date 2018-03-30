2a6e4a4926ca573f9d4a7a6fdcaf23658905a117
Tao Tao <ttao@thoughtworks.com>
2018-03-30 23:59:46 +0800

    add more Tree functions

--- Trees.hs
+++ Trees.hs
@@ -10,3 +10,11 @@ foldt f g (Bin x y) = g (foldt f g x) (foldt f g y)
 -- map
 tree:: (a -> b) -> Tree a -> Tree b
 tree f = foldt (\x -> Tip (f x)) Bin
+
+-- size and depth
+size = foldt one (+) where one a = 1
+depth = foldt zero (\x y -> succ (max x y)) where zero a = 0
+
+-- forests?
+-- data Tree' a = Fork a (Forest a)
+-- data Forest a = Null | Grow (tree' a) (Forest a)
